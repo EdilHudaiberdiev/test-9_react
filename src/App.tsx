a2b83d2c5556694c606655c949e0b76d7b5e8375
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2023-12-30 12:40:23 +0600

    create firebase, added axiosApi, added Ui components: Spinner, Toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css';
+import Toolbar from './Components/UI/ToolBar/Toolbar';
+import {Route, Routes} from 'react-router-dom';
+import Home from './Containers/Home/Home';
+
 
 function App() {
 
   return (
     <>
-        Content
+      <header>
+        <Toolbar/>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          {/*<Route path="/add-transaction" element={<AddNewDish/>}/>*/}
+          <Route path="*" element={(<h1>Not found</h1>)}/>
+        </Routes>
+      </main>
     </>
   );
 }
